0b1e2c7334dd9e1f1b3f6938605354c09be3469e
黄钰杰 <huangyujie@huangyujiedeiMac.local>
2017-06-11 13:59:21 +0800

    atom

--- app/App.js
+++ app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 class Main extends React.Component {
+
   render() {
     return (
       <div className="shopping-list">
@@ -9,6 +10,7 @@ class Main extends React.Component {
           <li>Node {process.versions.node}</li>
           <li>Chrome {process.versions.chrome}</li>
           <li>Electron {process.versions.electron}</li>
+
         </ul>
       </div>
     );

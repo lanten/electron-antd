2dc4570544d449a9a18c3ee9908105fddeda6441
lanten <974986907@qq.com>
2018-06-13 15:55:52 +0800

    sync

--- app/containers/LoggedIn.js
+++ app/containers/LoggedIn.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
-import { $ } from '../components/fn';
 
 
 export default class LoggedIn extends Component {
   constructor(props) {
     super(props)
-    console.log(this.props, this.props.location.state, global.a);
-
-    console.log($);
-
-    $.test(this)
-
-
   }
   render() {
     return (

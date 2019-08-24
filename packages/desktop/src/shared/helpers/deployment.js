1b954d6f05f012f487e4c1cf605a9887057c6672
Chris Fenos <chris.m.fenos@gmail.com>
2019-08-24 18:03:07 +0200

    sharing

--- packages/desktop/src/shared/helpers/deployment.js
+++ packages/desktop/src/shared/helpers/deployment.js
@@ -1,4 +1,4 @@
-import { DomainDao } from './contracts';
+import { DomainDao, DomainRegistry, AdvertisingDAO } from './contracts';
 import { ethers } from 'ethers';
 const { utils } = ethers;
 
@@ -7,18 +7,26 @@ export const deployContract = async (wallet, provider, hash, url, type) => {
   console.log({ DomainDao, wallet, provider, hash, url });
   console.log('====================================');
 
-  if (type === 'ad') {
-    try {
-      let factory = new ethers.ContractFactory(DomainDao.abi, DomainDao.bytecode, wallet);
-      let contract = await factory.deploy({ gasLimit: 8000000 });
-      // const response = await contract.deployed(); 
-      console.log('====================================');
-      // console.log({ factory, contract, response });
-      console.log({ factory, contract  });
-      console.log('====================================');
-    } catch (e) {
-      console.log({ e })
-    }
-   
-  }
+  let pKey = wallet.privateKey;
+  let w = new ethers.Wallet(pKey, 'https://mainnet.infura.io/v3/c4f1200594d142dc9927d5ea8c1672e6');
+
+  // let infuraProvider = new ethers.providers.InfuraProvider('https://mainnet.infura.io/v3/c4f1200594d142dc9927d5ea8c1672e6');
+  // console.log({ infuraProvider })
+
+  console.log('====================================');
+  console.log({ w  });
+  console.log('====================================');
+  // if (type === 'ad') {
+  //   try {
+  //     let factory = new ethers.ContractFactory(DomainDao.abi, DomainDao.bytecode, wallet);
+  //     let contract = await factory.deploy({ gasLimit: 8000000 });
+  //     // const response = await contract.deployed(); 
+  //     console.log('====================================');
+  //     // console.log({ factory, contract, response });
+  //     console.log({ factory, contract  });
+  //     console.log('====================================');
+  //   } catch (e) {
+  //     console.log({ e })
+  //   }
+  // }
 }
--- packages/desktop/src/shared/helpers/user.js
+++ packages/desktop/src/shared/helpers/user.js
@@ -2,7 +2,7 @@ import { ethers } from 'ethers';
 const { utils } = ethers;
 require('dotenv').config()
 
-// will create a random key pair (hardcoded wallet for now)
+// will create a random key pair
 export const generateWallet = (provider) => {
   return new ethers.Wallet(`${process.env.REACT_APP_PRIVATE_KEY}`, provider);
 }

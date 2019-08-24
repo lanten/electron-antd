9bcb3cd3eb6b93fdd1093ebb57821bebe10bf199
Chris Fenos <chris.m.fenos@gmail.com>
2019-08-24 17:41:16 +0200

    contracts imported

--- packages/desktop/src/shared/helpers/contracts.js
+++ packages/desktop/src/shared/helpers/contracts.js
@@ -1,7 +1,17 @@
 
 // will deploy contracts & save name, domain associated & address in db.
 export const DomainDao = require('../../../build/contracts/DomainDao');
+export const AdvertisingDAO = require('../../../build/contracts/AdvertisingDAO');
+export const DomainRegistry = require('../../../build/contracts/DomainRegistry');
+export const Escrow = require('../../../build/contracts/Escrow');
+export const PaymentSplitter = require('../../../build/contracts/PaymentSplitter');
+export const Secondary = require('../../../build/contracts/Secondary');
 
 export const contracts = {
+  domainRegistry: DomainRegistry,
   domain: DomainDao,
+  advertising: AdvertisingDAO,
+  escrow: Escrow,
+  paymentSplitter: PaymentSplitter,
+  secondary: Secondary
 };

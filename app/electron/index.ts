f99d1abb60eaecd21144a360214206e3c047f532
lanten <lanten233@qq.com>
2020-01-04 20:00:59 +0800

    移除 electron-react-devtools ，事实上它并没有什么卵用

--- app/electron/index.ts
+++ app/electron/index.ts
@@ -3,19 +3,6 @@ import { app, Tray } from 'electron'
 import { creatAppTray } from './tray'
 import { createWindow } from './window'
 
-const { NODE_ENV } = process.env
-
-if (NODE_ENV === 'development') {
-  // react-developer-tools
-  require('electron-debug')({ showDevTools: false })
-  app.on('ready', () => {
-    const installExtension = require('electron-devtools-installer')
-    installExtension.default(installExtension.REACT_DEVELOPER_TOOLS).catch((err: any) => {
-      console.log('Unable to install `react-developer-tools`: \n', err)
-    })
-  })
-}
-
 let tray: Tray
 
 app.on('ready', () => {

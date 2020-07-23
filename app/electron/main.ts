e81f846ef0574b165afc104335c2497cf915452a
lanten <lanten233@qq.com>
2020-07-23 11:04:37 +0800

    解决实例重复创建问题

--- app/electron/main.ts
+++ app/electron/main.ts
@@ -8,6 +8,18 @@ let tray: Tray
 
 app.allowRendererProcessReuse = true
 
+const appLock = app.requestSingleInstanceLock()
+
+if (!appLock) {
+  // 作为第二个实例运行时, 主动结束进程
+  app.quit()
+}
+
+app.on('second-instance', () => {
+  // 当运行第二个实例时, 打开或激活首页
+  $tools.createWindow('Home')
+})
+
 app.on('ready', () => {
   tray = creatAppTray()
   $tools.createWindow('Home')
